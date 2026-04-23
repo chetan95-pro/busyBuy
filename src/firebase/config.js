00d46f5c10e305e89cb17a5b758daddd48f4af6c
unknown <chetansonawane2695@gmail.com>
2026-04-23 23:58:26 +0530

    removed firebase keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUJP_i-j2znxx_9e27nmd-NrO9Pi4OJb0",
-  authDomain: "busybuy1-6e2b5.firebaseapp.com",
-  projectId: "busybuy1-6e2b5",
-  storageBucket: "busybuy1-6e2b5.firebasestorage.app",
-  messagingSenderId: "348702424333",
-  appId: "1:348702424333:web:f3883d616f5e6db14ec444",
-  measurementId: "G-WGECMLLHBB",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
